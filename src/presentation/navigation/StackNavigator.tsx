bc74323ce38b6c509539448f03d1c7d6af12e644
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-03-26 19:44:37 -0500

    inicio app

---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,38 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { LoadingScreen } from '../screens/loading/LoadingScreen';
+import { LoginScreen } from '../screens/auth/LoginScreen';
+import { RegisterScreen } from '../screens/auth/RegisterScreen';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { ProductsScreen } from '../screens/products/ProductsScreen';
+
+
+
+export type RootStackParam = {
+    LoadingScreen: undefined;
+    LoginScreen: undefined;
+    RegisterScreen: undefined;
+    HomeScreen: undefined;
+    ProductsScreen: undefined;
+}
+
+const Stack = createStackNavigator<RootStackParam>();
+
+export const StackNavigator = () => {
+
+
+
+
+  return (
+    <Stack.Navigator
+   screenOptions={{headerShown: false}}
+    >
+      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="ProductsScreen" component={ProductsScreen} />
+
+    </Stack.Navigator>
+  );
+};
---
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Component Content</Text>
+    </View>
+  );
+};
